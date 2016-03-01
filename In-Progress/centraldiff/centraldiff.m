b59dfec67de0ee760e892626df7abe1d74411180
BEC1 <bec1mailbox@gmail.com>
2016-03-01 10:34:58 -0500

    added accuracy 4

--- In-Progress/centraldiff/centraldiff.m
+++ In-Progress/centraldiff/centraldiff.m
@@ -31,13 +31,23 @@ end
 
 gridspacing = points(2) - points(1);
 
-diffcoeffs = [-1/2, 0, 1/2];
+switch accuracy
+    case 2
+        diffcoeffs = [-1/2, 0, 1/2];
+    case 4
+        diffcoeffs = [1/12, -2/3, 0, 2/3,-1/12];
+end
 paddeddata = padarray(data,[0 3],'replicate');
 diffout = data;
 
 for i=1:length(data)
     center = i+3;
-    neighborhood = paddeddata(center-1:center+1);
+    switch accuracy 
+        case 2
+            neighborhood = paddeddata(center-1:center+1);
+        case 4
+            neighborhood = paddeddata(center-2:center+2);
+    end
     diffout(i) = diffcoeffs*neighborhood' / gridspacing;
 end
         
