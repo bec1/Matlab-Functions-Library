f2913ffdb064811bdf0ed5b92e1d68c6da1490b3
ZhenjieYan <zhenjie@mit.edu>
2016-03-24 18:44:28 -0400

    add the finite differentiate function

---
+++ Final/MomentumRefocusing/Library/FiniteD.m
@@ -0,0 +1,14 @@
+function Out = FiniteD( x,y,SD )
+%FINITED Summary of this function goes here
+%   Detailed explanation goes here
+N=length(x);
+Out=n*0;
+
+for i=1:N
+    k1=max(1,i-SD);
+    k2=min(N,i+SD);
+    Out(i)=(y(k2)-y(k1))/(x(k2)-x(k1));
+end
+
+end
+
